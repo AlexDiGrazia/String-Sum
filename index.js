ff010c67dbeac62157201640a1ffbf590d70381e
Alex Digrazia <alexndigrazia@hotmail.com>
2022-09-06 14:43:30 -0700

    first working branch initial commit

--- index.js
+++ index.js
@@ -1,26 +1,4 @@
-function stringSum(str) {
-  var sum = 0;
-  var answer = str.match(/[1-9]/g);
- 
-  for (var i = 0; i < 6; i++)
 
-}
-
-
-
-
-
-var value = answer.at(5);
-
-
-for {
-  var number = Number(answer.at(i));
-  console.log(number);
-
-}
-
-var finalAnswer = stringSum("dk38f4n8ke92j");
-console.log(finalAnswer);
 
 
 
