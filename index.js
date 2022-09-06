0d4202bd1339172cef32970621cc0109ba01d721
Alex Digrazia <alexndigrazia@hotmail.com>
2022-09-06 14:37:17 -0700

    initial commit

---
+++ index.js
@@ -0,0 +1,37 @@
+function stringSum(str) {
+  var sum = 0;
+  var answer = str.match(/[1-9]/g);
+ 
+  for (var i = 0; i < 6; i++)
+
+}
+
+
+
+
+
+var value = answer.at(5);
+
+
+for {
+  var number = Number(answer.at(i));
+  console.log(number);
+
+}
+
+var finalAnswer = stringSum("dk38f4n8ke92j");
+console.log(finalAnswer);
+
+
+
+
+
+/* 
+document.getElementById("demo").innerHTML =  value; */
+
+
+/* 
+function sum(numberFirst, numberSecond) {
+  
+} */
+
