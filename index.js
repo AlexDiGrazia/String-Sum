892c52a844f17a2b09177879fd4f9c8474a42e03
Alex Digrazia <alexndigrazia@hotmail.com>
2022-09-06 18:39:19 -0700

    final solution for String Sum

--- index.js
+++ index.js
@@ -1,15 +1,19 @@
+function stringSum(str) {
 
+  var sum = 0;
+  var containsNumber = /\d/.test(str);
 
+  if (containsNumber) {
 
+    var extractNumber = str.match(/[0-9]/g);
 
+    for (i = 0; i < extractNumber.length; i++) {
+      var sum = sum + Number(extractNumber[i]);
+    }
+    console.log(sum);
+  } else {
+    console.log(sum);
+  }
+}
 
-
-/* 
-document.getElementById("demo").innerHTML =  value; */
-
-
-/* 
-function sum(numberFirst, numberSecond) {
-  
-} */
-
+var result = stringSum('sl7k2jm4kl7sdm9l sadf');
